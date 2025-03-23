be4de58499c31ec296ce9350698813c4b340ccd1
Mrudula Gavas <157680254+mrudulagavas@users.noreply.github.com>
2025-03-23 16:16:19 +0530

    Add files via upload

--- ai_search_ui/search.py
+++ ai_search_ui/search.py
@@ -1,3 +1,26 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7f921b3181598d433ea875b1ac97ef7b9617344bcabe5a1d54b2eb5a17e0f821
-size 787
+import requests
+
+def search_request(search_query, links_threshold=5, data_limit_per_page=10000):
+    print("going to search request")
+
+    base_url = 'http://localhost:3000/ai/search'
+    
+    # Prepare query parameters
+    params = {
+        'search_query': search_query,
+        'links_threshold': links_threshold,
+        'data_limit_per_page': data_limit_per_page
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response_data = response.json()
+        
+        if response.status_code == 200:
+            return response_data['data']
+        else:
+            print("Error:", response_data.get('error', 'Unknown error'))
+            return None
+    except Exception as e:
+        print("Error:", str(e))
+        return str(e)
--- ai_search_ui/trans.py
+++ ai_search_ui/trans.py
@@ -1,3 +1,38 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d68601c5a44d1e76fbcb3bd22ffa1020d8bc9d3b4296901701daac17509a82f3
-size 1090
+from gtts import gTTS
+import streamlit as st
+import base64
+import io
+
+
+# filename=generate_unique_audio_filename()
+# print("filename",filename)
+playback_speed=1.25
+
+def speak(text,language='en'):
+    try:
+        # text to speak code
+        tts = gTTS(text, lang=language, slow=False)
+        
+        mp3_fp = io.BytesIO()
+        tts.write_to_fp(mp3_fp)
+        mp3_fp.seek(0)  # Move the pointer to the start of the BytesIO object
+        b64 = base64.b64encode(mp3_fp.read()).decode('utf-8')
+
+        # displaying audio in html
+        md = f"""
+                <audio controls autoplay="true">
+                <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+                </audio>
+                <script>
+                    var audio = document.querySelector('audio');
+                    audio.playbackRate = {playback_speed};
+                </script>
+                """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
