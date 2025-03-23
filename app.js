2ddee34eb13ee12c0172f8a6d81f52437688dc1a
Mrudula Gavas <157680254+mrudulagavas@users.noreply.github.com>
2025-03-23 16:15:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5a13b867902bffce044426a0af4d1e7c407636b27ea188cc030a9407a7d04271
-size 988
+require('dotenv').config();
+require('./page_pool')
+
+const express = require('express');
+const { search } = require('./search_engine');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.get('/ai/search', async (req, res) => {
+    try {
+
+        const { search_query, links_threshold = 5, data_limit_per_page = 10000 } = req.query;
+
+        // console.log("search_query : ",search_query)
+        // You can modify this based on your actual search logic
+        const searchResult = await search(search_query, links_threshold, data_limit_per_page);
+
+        // Respond with the search result
+        return res.status(200).json({ success: true, data: searchResult });
+    } catch (err) {
+        console.log("Error while search : ", err);
+        return res.status(400).json({ error: err.message })
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
